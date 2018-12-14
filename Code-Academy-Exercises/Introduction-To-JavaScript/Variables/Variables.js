ceeee228c741bf73969d5c0531ae5220bd98824c
Alan Steenblock <alan.steenblock@gmail.com>
2018-12-13 18:05:09 -0700

    updated file

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,5 @@
+// In short, variables label and store data in memory. There are only a few things you can do with variables:
+//
+//     Create a variable with a descriptive name.
+//     Store or update information stored in a variable.
+//     Reference or “get” information stored in a variable.

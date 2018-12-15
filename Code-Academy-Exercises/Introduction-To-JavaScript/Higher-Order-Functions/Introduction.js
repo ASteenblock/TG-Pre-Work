a005a2dc0871ea370b935d8cfa7482094f0defe8
Alan Steenblock <alan.steenblock@gmail.com>
2018-12-15 11:25:34 -0700

    updated file

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Introduction.js
@@ -0,0 +1,12 @@
+// In programming, we can accomplish “abstraction” by writing functions. In
+// addition to allowing us to reuse our code, functions help to make clear,
+// readable programs. If you encountered countToThree() in a program, you might
+// be able to quickly guess what the function did without having to stop and read
+// the function's body.
+//
+// We're also going to learn about another way to add a level of abstraction to
+// our programming: higher-order functions. Higher-order functions are functions
+// that accept other functions as arguments and/or return functions as output.
+// This enables us to build abstractions on other abstractions, just like "We
+// hosted a birthday party" is an abstraction that may build on the abstraction
+// "We made a cake."

edb484dfd3dee83f374558acd39cc3c239e483a6
Alan Steenblock <alan.steenblock@gmail.com>
2018-12-15 12:42:29 -0700

    updated file

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-1.js
@@ -0,0 +1,5 @@
+// Because companies add support for ES6 features gradually, it's important for you 
+// to know how to look up browser support on a feature-by-feature basis. The
+// website caniuse.com is the best resource for finding browser compatibility
+// information.
+//

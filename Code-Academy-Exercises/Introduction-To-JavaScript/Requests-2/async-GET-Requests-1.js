8ddb6d79a9ffa6d7594a8747e4d92ad41ea0bd46
Alan Steenblock <alan.steenblock@gmail.com>
2018-12-15 14:30:05 -0700

    updated file

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-1.js
@@ -0,0 +1,7 @@
+// In the following exercises, we’re going to take what you’ve learned about
+// chaining Promises and make it simpler using functionality introduced in ES8:
+// async and await. You read that right, you did the hard part already, now
+// it’s time to make it easier.
+//
+// The structure for this request will also be slightly different. Notice the
+// new keywords async and await, as well as the try and catch statements.
